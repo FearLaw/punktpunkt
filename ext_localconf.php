1ee61caef7839ae552da79d1dc46fb63e3de322e
FearLaw <101412300+FearLaw@users.noreply.github.com>
2023-06-09 03:48:57 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,1 +1,15 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+    'ekhn',
+    'setup',
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ekhn/Configuration/TypoScript/setup.txt">'
+);
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Vendor\\MyExtension\\Task\\MySkript'] = [
+    'extension' => 'ekhn',
+    'title' => 'My Skript Task',
+    'description' => 'This task executes my custom script daily.',
+    'additionalFields' => ''
+];
